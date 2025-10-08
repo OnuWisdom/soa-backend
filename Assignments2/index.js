fb4a22e7110db390aefa6278700d23964a81c739
Onu Wisdom <onuwisdom2000@gmail.com>
2025-10-08 13:18:18 +0100

    no major change

--- Assignments2/index.js
+++ Assignments2/index.js
@@ -1,15 +1,12 @@
-// server.js
 const express = require('express');
 const app = express();
 
-// Route 1: /hello → returns greeting
 app.get('/hello', (req, res) => {
 	res.json({ status: true, msg: 'Hello there! Welcome to my Express app ' });
 });
 
-// Route 2: /greet/:name → returns “Hello, [name]”
 app.get('/greet/:name', (req, res) => {
-	const name = req.params.name; // get name from URL
+	const name = req.params.name;
 	res.json({ status: true, msg: `Hello, ${name}!` });
 });
 
